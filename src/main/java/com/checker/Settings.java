ecbe5e6dc7338f6151b567f7f269a88b37c65357
JulianKarrer <juliankarrer@t-online.de>
2020-10-24 18:01:12 +0200

    Update target SDK to 29 Migrate from legacy support library to androidx

--- src/main/java/com/checker/Settings.java
+++ src/main/java/com/checker/Settings.java
@@ -1,7 +1,7 @@
 package com.checker;
 
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Switch;
